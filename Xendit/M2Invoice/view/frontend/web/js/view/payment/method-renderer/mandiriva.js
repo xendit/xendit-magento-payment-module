acb93eb4a827aa73ac757cab8a936163f49a283b
hakiemaul <noersedya@gmail.com>
2019-04-30 12:03:35 +0700

    [TPI-95] mandiri and BRI done

--- Xendit/M2Invoice/view/frontend/web/js/view/payment/method-renderer/mandiriva.js
+++ Xendit/M2Invoice/view/frontend/web/js/view/payment/method-renderer/mandiriva.js
@@ -1,17 +1,35 @@
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Checkout/js/view/payment/default',
+        'mage/url',
+        'Magento_Checkout/js/model/quote'
     ],
-    function (Component) {
+    function (
+        Component,
+        url,
+        quote
+        ) {
         'use strict';
+
+        var self = this;
 
         return Component.extend({
             defaults: {
-                template: 'Xendit_M2Invoice/payment/invoiceva'
+                template: 'Xendit_M2Invoice/payment/invoiceva',
+                redirectAfterPlaceOrder: false,
+            },
+
+            initialize: function() {
+                this._super();
+                self = this;
             },
 
             getCode: function() {
                 return 'mandiriva';
+            },
+
+            getMethod: function() {
+                return 'MANDIRI'
             },
 
             getTest: function() {
@@ -36,8 +54,41 @@ define(
             },
 
             afterPlaceOrder: function () {
-                window.location.replace(url.build('xendit/checkout/invoice'));
+                window.location.replace(url.build(`xendit/checkout/invoice?preferred_method=${self.getMethod()}`));
             },
+
+            validate: function() {
+                var billingAddress = quote.billingAddress();
+                var totals = quote.totals();
+
+                self.messageContainer.clear();
+
+                if (!billingAddress) {
+                    self.messageContainer.addErrorMessage({'message': 'Please enter your billing address'});
+                    return false;
+                }
+
+                if (!billingAddress.firstname || 
+                    !billingAddress.lastname ||
+                    !billingAddress.street ||
+                    !billingAddress.city ||
+                    !billingAddress.postcode ||
+                    billingAddress.firstname.length == 0 ||
+                    billingAddress.lastname.length == 0 ||
+                    billingAddress.street.length == 0 ||
+                    billingAddress.city.length == 0 ||
+                    billingAddress.postcode.length == 0) {
+                    self.messageContainer.addErrorMessage({'message': 'Please enter your billing address details'});
+                    return false;
+                }
+
+                if (totals.grand_total < 11000) {
+                    self.messageContainer.addErrorMessage({'message': 'Xendit doesn\'t support purchases less than Rp 11.000.'});
+                    return false;
+                }
+
+                return true;
+            }
         });
     }
 );
