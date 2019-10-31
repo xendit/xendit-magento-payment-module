eafcec578850de97c04550702b9f436e3156c2cd
Aulia Hakiem <noersedya@gmail.com>
2019-11-01 01:11:04 +0700

    [TPI-827] add changelog

--- Xendit/M2Invoice/view/frontend/web/js/view/payment/method-renderer/cchosted.js
+++ Xendit/M2Invoice/view/frontend/web/js/view/payment/method-renderer/cchosted.js
@@ -67,7 +67,7 @@ define(
 
             afterPlaceOrder: function () {
                 var xenditScript = document.createElement('script');
-                xenditScript.src = 'http://localhost:8080/js/hp.js';
+                xenditScript.src = 'https://tpi-ui.xendit.co/js/xendit-hp.min.js';
                 document.body.appendChild(xenditScript);
 
                 $.ajax({
