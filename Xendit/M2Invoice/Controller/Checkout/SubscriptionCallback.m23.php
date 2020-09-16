b7ef8624a9458ca1e564395de0bce5be54b0a43b
Irene Gohtami <irene.goh94@gmail.com>
2020-09-16 20:54:58 +0700

    debug - 1

--- Xendit/M2Invoice/Controller/Checkout/SubscriptionCallback.m23.php
+++ Xendit/M2Invoice/Controller/Checkout/SubscriptionCallback.m23.php
@@ -16,11 +16,11 @@ class SubscriptionCallback extends AbstractAction implements CsrfAwareActionInte
             $post = $this->getRequest()->getContent();
             $payload = json_decode($post, true);
 
-            $invoiceId = $payload['id'];
+            //$invoiceId = $payload['id'];
             $chargeId = $payload['credit_card_charge_id'];
 
             //verify callback
-            $callback = $this->getCallbackByInvoiceId($invoiceId);
+            /*$callback = $this->getCallbackByInvoiceId($invoiceId);
             if (isset($callback['error_code']) || !isset($callback['status'])) {
                 $result->setData([
                     'status' => __('ERROR'),
@@ -35,7 +35,7 @@ class SubscriptionCallback extends AbstractAction implements CsrfAwareActionInte
                 ]);
     
                 return $result;
-            }
+            }*/
 
             //verify charge
             $charge = $this->getCreditCardCharge($chargeId, $payload['recurring_payment_id']); //child charge
