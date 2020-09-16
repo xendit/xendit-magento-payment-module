2de31f7d2f1fbc876192c8efbd0295137a9a7b58
Irene Gohtami <irene.goh94@gmail.com>
2020-09-16 10:06:06 +0700

    show subscription info on cc_subscription order

--- Xendit/M2Invoice/Block/CustomView.php
+++ Xendit/M2Invoice/Block/CustomView.php
@@ -1,24 +1,45 @@
 <?php
 namespace Xendit\M2Invoice\Block;
 
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
 use Xendit\M2Invoice\Helper\Data;
 
-class CustomView extends \Magento\Framework\View\Element\Html\Link
+class CustomView extends Template
 {
     private $dataHelper;
 
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
+        Context $context,
+        Registry $registry,
         Data $dataHelper,
         array $data = []
     ) {
+        $this->registry = $registry;
         $this->dataHelper = $dataHelper;
 
         parent::__construct($context, $data);
     }
 
+    /**
+     * Retrieve current order model instance
+     *
+     * @return \Magento\Sales\Model\Order
+     */
+    public function getOrder()
+    {
+        return $this->registry->registry('current_order');
+    }
+
+    public function getPaymentMethod()
+    {
+        return $this->getOrder()->getPayment()->getMethodInstance()->getCode();
+    }
+
     public function getSubscriptionConfig()
     {
+        $data = array();
         $data['card_subscription_interval'] = $this->dataHelper->getSubscriptionInterval();
         $data['card_subscription_interval_count'] = $this->dataHelper->getSubscriptionIntervalCount();
 
