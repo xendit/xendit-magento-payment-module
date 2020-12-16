0dfa044bb71455d0d5b5a02f681bdda4d060a2d5
candra saputra <candrasaputra@live.com>
2020-12-16 09:18:45 +0700

    update version

--- Xendit/M2Invoice/Helper/ApiRequest.php
+++ Xendit/M2Invoice/Helper/ApiRequest.php
@@ -85,7 +85,7 @@ class ApiRequest
             'Content-Type' => 'application/json',
             'x-plugin-name' => 'MAGENTO2',
             'user-agent' => 'Magento 2 Module',
-            'x-plugin-version' => '2.4.4'
+            'x-plugin-version' => '2.5.0'
         ];
 
         if ($preferredMethod !== null) {
