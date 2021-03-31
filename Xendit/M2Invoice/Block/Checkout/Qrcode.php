2f8ec1ac06ad289d8b20d715220ad6f56ee7173e
yanoandri <yano@xendit.co>
2021-03-31 14:30:37 +0700

    remove anton comment

--- Xendit/M2Invoice/Block/Checkout/Qrcode.php
+++ Xendit/M2Invoice/Block/Checkout/Qrcode.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Copyright © 2020 PT Kemana Teknologi Solusi. All rights reserved.
- * http://www.kemana.com
- */
-
-/**
- * @category Kemana
- * @package  Kemana_Xendit
- * @license  http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *
- *
- * @author   Anton Vinoj <avinoj@kemana.com>
- */
 
 namespace Xendit\M2Invoice\Block\Checkout;
 
