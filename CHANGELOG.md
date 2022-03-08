7957c69fc52499cbf5c7e1a200c49ea3463d578b
= <=>
2022-03-08 02:15:04 +0000

    Change version to 3.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # CHANGELOG
 
-## 3.4.1 (2022-03-08)
+## 3.5.0 (2022-03-08)
 Improvements:
 - Code refactoring
 - Update extension compatible with Magento 2.4.3
--- Xendit/M2Invoice/Helper/ApiRequest.php
+++ Xendit/M2Invoice/Helper/ApiRequest.php
@@ -110,7 +110,7 @@ class ApiRequest
             'Content-Type' => 'application/json',
             'x-plugin-name' => 'MAGENTO2',
             'user-agent' => 'Magento 2 Module',
-            'x-plugin-version' => '3.4.1'
+            'x-plugin-version' => '3.5.0'
         ];
 
         if ($preferredMethod !== null) {
