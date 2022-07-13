7c7ad6faa29b341ae50c609e8808a7d94a4d6ed6
andy <andy.nguyen@xendit.co>
2022-07-13 03:45:45 +0000

    Fix multi shipping create spam invoice

--- Xendit/M2Invoice/Controller/Checkout/Notification.php
+++ Xendit/M2Invoice/Controller/Checkout/Notification.php
@@ -75,6 +75,9 @@ class Notification extends Action implements CsrfAwareActionInterface
      */
     private $xenditModel;
 
+    /**
+     * @var OrderRepository
+     */
     private $orderRepository;
 
     /**
