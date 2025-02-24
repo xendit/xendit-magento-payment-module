62200171e3838f791a5590f6037ff01e86010211
Yano Andriyanto <yano@xendit.co>
2025-02-24 13:22:25 +0700

    use only serializer json

--- Model/Payment/AbstractInvoice.php
+++ Model/Payment/AbstractInvoice.php
@@ -16,7 +16,7 @@ use Magento\Store\Model\StoreManagerInterface;
 use Xendit\M2Invoice\Helper\ApiRequest;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Framework\Serialize\Serializer\Json as MagentoSerializerJson;
-use Xendit\M2Invoice\External\Serialize\Serializer\Json  as XenditSerializerJson;
+// use Xendit\M2Invoice\External\Serialize\Serializer\Json  as XenditSerializerJson;
 use Magento\Quote\Api\Data\CartInterface;
 use Magento\Customer\Model\Session as CustomerSession;
 use Xendit\M2Invoice\Helper\Metric;
@@ -109,7 +109,6 @@ class AbstractInvoice extends AbstractMethod
      * @param RuleRepository $ruleRepo
      * @param CartRepositoryInterface $quoteRepository
      * @param MagentoSerializerJson $magentoSerializerJson
-     * @param XenditSerializerJson $xenditSerializerJson
      * @param CustomerSession $customerSession
      */
     public function __construct(
@@ -126,7 +125,7 @@ class AbstractInvoice extends AbstractMethod
         RuleRepository $ruleRepo,
         CartRepositoryInterface $quoteRepository,
         MagentoSerializerJson $magentoSerializerJson,
-        XenditSerializerJson $xenditSerializerJson,
+        // XenditSerializerJson $xenditSerializerJson,
         CustomerSession $customerSession,
         Metric $metricHelper
     ) {
@@ -147,15 +146,15 @@ class AbstractInvoice extends AbstractMethod
         $this->ruleRepo                 = $ruleRepo;
         $this->quoteRepository          = $quoteRepository;
         $this->magentoSerializerJson    = $magentoSerializerJson;
-        $this->xenditSerializerJson     = $xenditSerializerJson;
+        // $this->xenditSerializerJson     = $xenditSerializerJson;
         $this->customerSession          = $customerSession;
         $this->metricHelper             = $metricHelper;
 
-        if (interface_exists("Magento\Framework\Serialize\Serializer\Json")) {
+        // if (interface_exists("Magento\Framework\Serialize\Serializer\Json")) {
             $this->serializer = $this->magentoSerializerJson;
-        } else {
-            $this->serializer = $this->xenditSerializerJson;
-        }
+        // } else {
+        //     $this->serializer = $this->xenditSerializerJson;
+        // }
     }
 
     /**
