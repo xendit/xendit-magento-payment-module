31abfdf35684bf54f6b24a329fb14090ad8f142d
Yano Andriyanto <yano@xendit.co>
2025-02-24 13:27:42 +0700

    remove xendit serializer all at once

--- Model/Adminhtml/Source/CartRule.php
+++ Model/Adminhtml/Source/CartRule.php
@@ -5,7 +5,6 @@ namespace Xendit\M2Invoice\Model\Adminhtml\Source;
 use Magento\Framework\Model\Context;
 use Magento\SalesRule\Model\ResourceModel\Rule\CollectionFactory;
 use Magento\Framework\Serialize\Serializer\Json as MagentoSerializerJson;
-// use Xendit\M2Invoice\External\Serialize\Serializer\Json  as XenditSerializerJson;
 
 /**
  * Class CartRule
@@ -52,18 +51,12 @@ class CartRule implements \Magento\Framework\Option\ArrayInterface
     public function __construct(
         Context $context,
         CollectionFactory $ruleCollectionFactory,
-        MagentoSerializerJson $magentoSerializerJson,
-        // XenditSerializerJson $xenditSerializerJson
+        MagentoSerializerJson $magentoSerializerJson
     ) {
         $this->ruleCollectionFactory = $ruleCollectionFactory;
         $this->cache = $context->getCacheManager();
         $this->magentoSerializerJson = $magentoSerializerJson;
-        $this->xenditSerializerJson = $xenditSerializerJson;
-        // if (interface_exists("Magento\Framework\Serialize\Serializer\Json")) {
-            $this->serializer = $this->magentoSerializerJson;
-        // } else {
-        //     $this->serializer = $this->xenditSerializerJson;
-        // }
+        $this->serializer = $this->magentoSerializerJson;
     }
 
     /**
--- Model/Payment/AbstractInvoice.php
+++ Model/Payment/AbstractInvoice.php
@@ -16,7 +16,6 @@ use Magento\Store\Model\StoreManagerInterface;
 use Xendit\M2Invoice\Helper\ApiRequest;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Framework\Serialize\Serializer\Json as MagentoSerializerJson;
-// use Xendit\M2Invoice\External\Serialize\Serializer\Json  as XenditSerializerJson;
 use Magento\Quote\Api\Data\CartInterface;
 use Magento\Customer\Model\Session as CustomerSession;
 use Xendit\M2Invoice\Helper\Metric;
@@ -146,15 +145,9 @@ class AbstractInvoice extends AbstractMethod
         $this->ruleRepo                 = $ruleRepo;
         $this->quoteRepository          = $quoteRepository;
         $this->magentoSerializerJson    = $magentoSerializerJson;
-        // $this->xenditSerializerJson     = $xenditSerializerJson;
         $this->customerSession          = $customerSession;
         $this->metricHelper             = $metricHelper;
-
-        // if (interface_exists("Magento\Framework\Serialize\Serializer\Json")) {
-            $this->serializer = $this->magentoSerializerJson;
-        // } else {
-        //     $this->serializer = $this->xenditSerializerJson;
-        // }
+        $this->serializer               = $this->magentoSerializerJson;
     }
 
     /**
