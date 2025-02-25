567290d21c65e2f271f55e9b5ecbdec519af94f3
Yano Andriyanto <yano@xendit.co>
2025-02-25 10:56:21 +0700

    Add External folder on release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Release script started"
 echo "Make sure to already follow the guideline before submit at https://xendit.atlassian.net/wiki/spaces/TPI/pages/356122802/Magento+Publish+Guide"
-FILES_TO_ZIP="Block Controller etc Gateway Helper Logger Model Plugin Setup Test view CHANGELOG.md LICENSE README.md registration.php composer.json"
+FILES_TO_ZIP="Block Controller etc External Gateway Helper Logger Model Plugin Setup Test view CHANGELOG.md LICENSE README.md registration.php composer.json"
 
 # Create a zip file
 ZIP_FILE="release.zip"
