91fe21d2827290c6c79947b44e2721db3dd2afd7
Sean Yu <seanyu.dev@gmail.com>
2025-11-03 14:46:17 +0800

    not show when not enabled in admin

--- Model/Payment/Unified.php
+++ Model/Payment/Unified.php
@@ -3,9 +3,10 @@
 namespace Xendit\M2Invoice\Model\Payment;
 
 use Magento\Payment\Model\Method\AbstractMethod;
+use Magento\Quote\Api\Data\CartInterface;
 
 /**
- * Class Akulaku
+ * Class Unified
  * @package Xendit\M2Invoice\Model\Payment
  */
 class Unified extends AbstractMethod
@@ -24,4 +25,27 @@ class Unified extends AbstractMethod
      */
     protected $_code = 'unified';
     protected $methodCode = 'UNIFIED';
+
+    /**
+     * Check whether payment method is available
+     *
+     * @param CartInterface|null $quote
+     * @return bool
+     */
+    // FUTURE TODO: refactor Unified and xendit to be one class only
+    public function isAvailable(?CartInterface $quote = null): bool
+    {
+        // First check if parent method allows availability
+        if (!parent::isAvailable($quote)) {
+            return false;
+        }
+
+        // Check if Xendit is enabled via the main active configuration
+        $isXenditActive = $this->_scopeConfig->getValue(
+            'payment/xendit/active',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+
+        return (bool) $isXenditActive;
+    }
 }
